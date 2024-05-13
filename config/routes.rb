bbf41316fd96fee15d5f2fc091cfde8a978f029b
muunimoz <160251803+muunimoz@users.noreply.github.com>
2024-05-13 11:15:46 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :admins
+  
+  root to: "homes#top"
+   get "about" => "homes#about"
+  
+ 
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
